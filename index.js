bf10fced426bffa724dfce20a9f9ced050b57dab
Marius13z <lemonlcsnet@yahoo.com>
2022-11-10 09:07:53 +0200

    test-cors

--- index.js
+++ index.js
@@ -11,21 +11,21 @@ import cookieParser from "cookie-parser";
 const app = express();
 dotenv.config();
 
-let whitelist = ["http://localhost:3000", "http://192.168.1.2:3000", "https://taupe-mousse-6ba3b1.netlify.app/"];
-let corsOptions = {
-  origin: (origin, callback) => {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true,
-};
+// let whitelist = ["http://localhost:3000", "http://192.168.1.2:3000", "https://taupe-mousse-6ba3b1.netlify.app"];
+// let corsOptions = {
+//   origin: (origin, callback) => {
+//     if (whitelist.indexOf(origin) !== -1) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error("Not allowed by CORS"));
+//     }
+//   },
+//   credentials: true,
+// };
 // parse cookies
 app.use(cookieParser());
 
-app.use(cors(corsOptions));
+app.use(cors());
 
 // Set up bodyparser to store the body of a req in req.body
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
@@ -34,7 +34,7 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 // post routes
 app.use("/posts", postRoutes);
 app.use("/user", userRoutes);
-app.use("/", async (req, res) => (res.json("It's working!")))
+
 
 // create PORT
 const PORT = process.env.PORT;
